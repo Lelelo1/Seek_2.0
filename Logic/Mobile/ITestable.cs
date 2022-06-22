222c48e37d8a1c6917ada133ce4101f63d8ca570
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-06-22 19:47:34 +0200

    linked in secrets

--- Logic/Mobile/ITestable.cs
+++ Logic/Mobile/ITestable.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 namespace XamarinLogic
 {
-    // never mind actually, just try catch the Init methods instead..?
-    /*
-    public interface ITestable
-    {
-        bool Assertion { get; }
-        bool Test();
-    }
+	// never mind actually, just try catch the Init methods instead..?
+	/*
+	public interface ITestable
+	{
+		bool Assertion { get; }
+		bool Test();
+	}
 
-    // https://stackoverflow.com/questions/37093261/attach-stacktrace-to-exception-without-throwing-in-c-sharp-net
-    public class TestException : Exception
-    {
+	// https://stackoverflow.com/questions/37093261/attach-stacktrace-to-exception-without-throwing-in-c-sharp-net
+	public class TestException : Exception
+	{
 
-        public TestException(string message, string stackTrace) : base(message)
-        {
-            StackTrace = stackTrace;
-        }
+		public TestException(string message, string stackTrace) : base(message)
+		{
+			StackTrace = stackTrace;
+		}
 
-        public override string StackTrace { get; }
-    }
-    */
+		public override string StackTrace { get; }
+	}
+	*/
 }
