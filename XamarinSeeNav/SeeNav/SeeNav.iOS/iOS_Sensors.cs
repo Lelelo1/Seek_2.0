874850ba3ccf72fcdaa9f708d4f307bcd967b702
Lelelo1 <42244983+Lelelo1@users.noreply.github.com>
2022-07-08 18:17:19 +0200

    added gyro and mag sensor code to ios

---
+++ XamarinSeeNav/SeeNav/SeeNav.iOS/iOS_Sensors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CoreMotion;
+using Foundation;
+using LogicLibrary.Services;
+using LogicLibrary;
+using System.Numerics;
+
+namespace SeeNav.iOS
+{
+    public class iOS_Sensors
+    {
+        CMMotionManager MotionManager { get; }
+
+        public iOS_Sensors()
+        {
+
+            MotionManager.StartDeviceMotionUpdates(CMAttitudeReferenceFrame.XTrueNorthZVertical, NSOperationQueue.CurrentQueue, CMDeviceMotionHandler);
+        }
+
+        
+
+        CMDeviceMotionHandler CMDeviceMotionHandler { get; } = new CMDeviceMotionHandler(SetSensorReading);
+
+        static SensorFusion SensorFusion => Logic.DependencyBox.Get<SensorFusion>();
+
+        static void SetSensorReading(CMDeviceMotion deviceMotion, NSError error)
+        {
+            var g = deviceMotion.RotationRate;
+            var m = deviceMotion.MagneticField.Field;
+            SensorFusion.SetGyroscope(new Vector3((float)g.x, (float)g.y, (float)g.z));
+            SensorFusion.SetMagnetometer(new Vector3((float)m.X, (float)m.Y, (float)m.Z));
+        }
+    }
+}
+
